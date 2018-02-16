ed29b407ee7a3b0f20c41ebba09d5f7403fbc5e3
Thiago Bianeck <bianeck27@gmail.com>
2018-02-15 22:03:10 -0200

    Aula 03 - Conteúdo Final
    Arquivos da AUla 03 - Cadastro de preços do produto

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/models/Produto.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/models/Produto.java
@@ -1,5 +1,8 @@
 package br.com.casadocodigo.loja.models;
 
+import java.util.List;
+
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,9 +14,13 @@ public class Produto {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
+	
 	private String titulo;
 	private String descricao;
 	private int paginas;
+	
+	@ElementCollection
+	private List<Preco> precos;
 	
 	
 	public String getTitulo() {
@@ -35,6 +42,18 @@ public class Produto {
 		this.paginas = paginas;
 	}
 	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public List<Preco> getPrecos() {
+		return precos;
+	}
+	public void setPrecos(List<Preco> precos) {
+		this.precos = precos;
+	}
 	@Override
 	public String toString() {
 		return "Produto [titulo=" + titulo + ",descricao=" + descricao + ",paginas=" 
